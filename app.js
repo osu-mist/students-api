63091d7f2535768d6a5540e50cbbd15e38e97798
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2019-02-20 09:55:14 -0800

    Change data source type

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const { validateDataSource } = appRoot.require('utils/validate-data-source');
 
 const serverConfig = config.get('server');
 
-validateDataSource('json');
+validateDataSource('oracledb');
 
 /**
  * @summary Initialize Express applications and routers
