6979e36f9854a11964550c223db72e5aad4beb05
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2019-03-13 16:30:50 -0700

    WIP - combine spy into promise testing

--- tests/unit/students-dao.test.js
+++ tests/unit/students-dao.test.js
@@ -28,11 +28,12 @@ describe('Test students-dao', () => {
   describe('Test getResourceById', () => {
     const fakeId = 'fakeId';
     const fakeStudentsSerializer = (rows, id, params) => rows; // eslint-disable-line no-unused-vars
+    const fakeParams = {};
 
     it(`should be fulfilled if
           1. isSingleton is true and only get exact one result
           2. isSingleton is false and get a list of results`, () => {
-      const fakeParams = {};
+      const spy = sinon.spy(fakeStudentsSerializer);
       const fulfilledCases = [
         { fakeSql: () => 'singleResult', isSingleton: true, expectResult: {} },
         { fakeSql: () => 'singleResult', isSingleton: false, expectResult: [{}] },
@@ -41,51 +42,29 @@ describe('Test students-dao', () => {
 
       const fulfilledPromises = [];
       _.each(fulfilledCases, ({ fakeSql, isSingleton, expectResult }) => {
-        const result = studentsDao.getResourceById(
-          fakeId,
-          fakeSql,
-          fakeStudentsSerializer,
-          isSingleton,
-          fakeParams,
-        );
+        const result = studentsDao.getResourceById(fakeId, fakeSql, spy, isSingleton, fakeParams);
         fulfilledPromises.push(result.should
           .to.eventually.be.fulfilled
-          .and.deep.equal(expectResult));
+          .and.deep.equal(expectResult)
+          .then(() => {
+            sinon.assert.alwaysCalledWithExactly(spy, any, any, fakeParams);
+            sinon.assert.calledThrice(spy);
+          }));
       });
       return Promise.all(fulfilledPromises);
     });
 
     it('should be rejected if isSingleton is true but get multiple results', () => {
       const fakeSql = () => 'multiResults';
+      const spy = sinon.spy(fakeStudentsSerializer);
       const isSingleton = true;
       const expectResult = 'Expect a single object but got multiple results.';
-      const fakeParams = {};
 
-      const result = studentsDao.getResourceById(
-        fakeId,
-        fakeSql,
-        fakeStudentsSerializer,
-        isSingleton,
-        fakeParams,
-      );
-      return result.should.to.eventually.be.rejectedWith(expectResult).and.be.an.instanceOf(Error);
-    });
-
-    it('should call the serializer and pass the params', async () => {
-      const fakeSql = () => 'multiResults';
-      const isSingleton = false;
-      const fakeParams = { key: 'value' };
-      const spy = sinon.spy(fakeStudentsSerializer);
-
-      await studentsDao.getResourceById(
-        fakeId,
-        fakeSql,
-        spy,
-        isSingleton,
-        fakeParams,
-      );
-      sinon.assert.calledOnce(spy);
-      sinon.assert.calledWithExactly(spy.getCall(0), any, any, fakeParams);
+      const result = studentsDao.getResourceById(fakeId, fakeSql, spy, isSingleton, fakeParams);
+      return result.should
+        .to.eventually.be.rejectedWith(expectResult)
+        .and.be.an.instanceOf(Error)
+        .then(() => sinon.assert.notCalled(spy));
     });
   });
 });
